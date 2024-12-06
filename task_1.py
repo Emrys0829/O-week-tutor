16b0cb4b2b99103a0c6a844dad92d4450d2985e1
Emrys Phạm <92245516+Emrys0829@users.noreply.github.com>
2024-12-06 16:42:53 +0700

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -7,6 +7,7 @@
 An integer 8745 can be rewrited as 8000 + 700 + 40 + 5
 The digit of the first number of the list is length of the list minus one and the digit of the second number is the length of the list minus two and so on until the last number
 To go through each number of the list we use a for loop and for each loop we add the numbers together.
+In this program I personally decide to write as a function so that we can reuse it or import it in the future.
 """
 
 def convert_to_int(list_of_numbers: list) -> int:
